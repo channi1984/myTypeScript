e2dd843e6ca2f3b0e3a9993ebecdda20eff4d4bc
Son Dong Chan <channi.kr@gmail.com>
2020-03-12 17:05:17 +0900

    1

--- test.js
+++ test.js
@@ -1,8 +1,31 @@
 // function greeter(person:string) {
 //     return "Hell," + person
 // }
-function greeter(person) {
-    return "Hello," + person.firstName + "" + person.lastName;
-}
-var user = { firstName: "Jane", lastName: "User" };
-document.body.innerHTML = greeter(user);
+// let user = [0,1,2];
+// document.body.innerHTML = greeter(user);
+//인터페이스
+// interface Person {
+//     firstName : string;
+//     lastName : string;
+// }
+// function greeter(person:Person) {
+//     return "Hello," + person.firstName + ""+person.lastName;
+// }
+// let user = {firstName: "Jane", lastName:"User"};
+// document.body.innerHTML = greeter(user);
+//Class 상속
+// class Student {
+//     fullname: string;
+//     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//         this.fullname = firstName + "" + middleInitial + "" + lastName;
+//     }
+// }
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + "" + person.lastName;
+// }
+// let user = new Student("Jane", "M.","User");
+// document.body.innerHTML= greeter(user);
--- test.ts
+++ test.ts
@@ -1,12 +1,5 @@
-// function greeter(person:string) {
-//     return "Hell," + person
-// }
+// //인터페이스
 
-// let user = [0,1,2];
-
-// document.body.innerHTML = greeter(user);
-
-//인터페이스
 // interface Person {
 //     firstName : string;
 //     lastName : string;
@@ -20,3 +13,42 @@
 
 // document.body.innerHTML = greeter(user);
 
+// //Class 상속
+
+// class Student {
+//     fullname: string;
+//     constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+//         this.fullname = firstName + "" + middleInitial + "" + lastName;
+//     }
+// }
+
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+// }
+
+// function greeter(person: Person) {
+//     return "Hello, " + person.firstName + "" + person.lastName;
+// }
+
+// let user = new Student("Jane", "M.","User");
+
+// document.body.innerHTML= greeter(user);
+
+// //Boolean
+// let isDone: boolean = false;
+
+// //Number
+// let decimal: number = 6;
+// let hex: number = 0xf00d;
+// let binary: number = 0b1010;
+// let octal: number = 0o744;
+
+// //String
+// let fullname: string = 'Bob Bobbignton';
+// let age: number = 37;
+// let sectece: string = `Hello, my name is ${fullname}. i'll be ${age + 1} years old next month`;
+
+// //Array
+// let list: number[] = [1,2,3];
+// let list: Array<number> = [1,2,3];
