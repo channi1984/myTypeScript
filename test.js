f2ea0d7f359a69ee0bd7c15f2187bf9aa9ec72fc
Son Dong Chan <channi.kr@gmail.com>
2020-04-08 10:58:31 +0900

    a

--- test.js
+++ test.js
@@ -13,3 +13,10 @@ var arrNum = [0, 1, 2, 3, 4, 5];
 var arrString = ["바나나", "귤", "사과"];
 //튜플
 var tpArray = ["바나나", 1];
+//객체
+var user = { name: '차니', height: 178 };
+//객체 선택속성
+var userNo = { name: '차니' };
+function getUser(types) {
+}
+getUser('channi');
--- test.ts
+++ test.ts
@@ -25,3 +25,20 @@ const arrString: string[] = ["바나나", "귤", "사과"];
 //튜플
 
 const tpArray: [string, number] = ["바나나", 1];
+
+//객체
+
+const user: {name:string; height:number;} = {name:'차니', height:178};
+
+//객체 선택속성
+const userNo : {name:string; height?:number} = {name:'차니'}
+
+
+//타입
+
+type types = string;
+function getUser(types:types) {
+
+}
+
+getUser('channi');
